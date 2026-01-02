035d61e9329c86636b1a5901d4b7908ec33ccfd4
Maria <mariaqibtyya1170@gmail.com>
2026-01-02 22:31:47 +0500

    fixed UI for homepage and themes

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -3,15 +3,21 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
-    scaffoldBackgroundColor: Colors.transparent,
-    primaryColor: Colors.deepPurple,
+    primaryColor: const Color(0xFF5D1B80),
+    scaffoldBackgroundColor: Colors.transparent, 
     fontFamily: 'Poppins',
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(color: Color(0xFF5D1B80)),
+    ),
   );
 
   static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
-    scaffoldBackgroundColor: Colors.black,
-    primaryColor: Colors.deepPurple,
+    primaryColor: Colors.deepPurpleAccent,
+    scaffoldBackgroundColor: Colors.transparent,
     fontFamily: 'Poppins',
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(color: Colors.white),
+    ),
   );
 }
--- lib/core/theme/theme_provider.dart
+++ lib/core/theme/theme_provider.dart
@@ -3,16 +3,23 @@ import 'app_theme.dart';
 
 class ThemeProvider extends ChangeNotifier {
   bool _isDark = false;
-
-  ThemeData get currentTheme =>
-      _isDark ? AppTheme.darkTheme : AppTheme.lightTheme;
+  double _fontSizeMultiplier = 1.0;
 
   bool get isDark => _isDark;
-  bool get isDarkMode => _isDark;
+  bool get isDarkMode => _isDark; 
+  double get fontSizeMultiplier => _fontSizeMultiplier;
+
+  ThemeData get currentTheme => _isDark ? AppTheme.darkTheme : AppTheme.lightTheme;
 
   void toggleTheme() {
     _isDark = !_isDark;
     notifyListeners();
   }
-}
 
+  void setFontSize(String size) {
+    if (size == 'Small') _fontSizeMultiplier = 0.8;
+    else if (size == 'Medium') _fontSizeMultiplier = 1.0;
+    else if (size == 'Large') _fontSizeMultiplier = 1.3;
+    notifyListeners();
+  }
+}
